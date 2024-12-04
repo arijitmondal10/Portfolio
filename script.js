09b02664ee940bf808229ff718c4aae8cd5c88a3
Arijit Mondal <153080677+arijitmondal10@users.noreply.github.com>
2024-12-05 00:05:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Programmer" ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+  
+
+function toggleNav() {
+    const navbar = document.querySelector('.navbar1');
+    navbar.classList.toggle('show');
+}
+
+function closeNavOnScroll() {
+    const navbar = document.querySelector('.navbar1');
+    if (navbar.classList.contains('show')) {
+        navbar.classList.remove('show');
+    }
+}
+
+window.addEventListener('scroll', closeNavOnScroll);
+  
